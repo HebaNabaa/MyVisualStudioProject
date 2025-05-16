3d3851aa0b4fbbd21aa78b4ed8df4078c3d4318d
Dareen Alherbawi <dado1-7-2003@hotmail.com>
2025-05-16 10:51:57 +0400

    Dareen commited

--- src/app/data/slotschedule.ts
+++ src/app/data/slotschedule.ts
@@ -1,9 +1,11 @@
 export class SlotSchedule {
   slot : string;
+  day: string;  
   time: string;
 
-  constructor(slot: string, time: string){
+  constructor(slot: string,day: string,time: string){
     this.slot = slot;
+     this.day = day;
     this.time = time;
   }
 }
