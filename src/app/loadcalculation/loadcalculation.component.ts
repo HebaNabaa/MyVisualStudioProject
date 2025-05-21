800819be85fbc7d1a47880576d3822b4323a021e
Heba Naba <hebaanbaa@gmail.com>
2025-05-21 22:11:11 +0400

    changed

--- src/app/loadcalculation/loadcalculation.component.ts
+++ src/app/loadcalculation/loadcalculation.component.ts
@@ -7,6 +7,7 @@ import { environment } from '../environments/environment';
 import { DataSnapshot, getDatabase, onValue, ref } from 'firebase/database';
 import { ScheduleSessionsComponent } from '../features/schedule-sessions/schedule-sessions.component';
 import { OnInit } from '@angular/core';
+import { count } from 'rxjs';
 @Component({
   selector: 'app-loadcalculation',
   standalone: true,
@@ -20,37 +21,87 @@ import { OnInit } from '@angular/core';
 export class LoadcalculationComponent implements OnInit {
   surgeons: string[] = [];
   db:any;
-  drLynnCount: number;
+drDareenCount: number;
+drHebaCount: number;
+drLynnCount: number;
+drMohammadCount: number;
+drAhmadCount: number;
+drLaylaCount: number;
+drKiranCount: number;
+drFaheemCount: number;
+drZakaryaCount: number;
+drAliCount: number;
 
   constructor() {
-    this.drLynnCount=0;
-    const app = initializeApp(environment);
-    this.db = getDatabase(app);
-  }
+this.drDareenCount = 0;
+this.drHebaCount = 0;
+this.drLynnCount = 0;
+this.drMohammadCount = 0;
+this.drAhmadCount = 0;
+this.drLaylaCount = 0;
+this.drKiranCount = 0;
+this.drFaheemCount = 0;
+this.drZakaryaCount = 0;
+this.drAliCount = 0;
+  const app = initializeApp(environment);
+  this.db = getDatabase(app);
+}
 
-  ngOnInit(): void {
-    const surgeonsList: string[] = [];
+ngOnInit(): void {
+  const surgeonsList: string[] = [];
 
-    onValue(ref(this.db, 'items'), (snapshot) => {
-      const data = snapshot.val();
+  let count1 = 0;
+  let count2 = 0;
+  let count3 = 0;
+  let count4 = 0;
+  let count5 = 0;
+  let count6 = 0;
+  let count7 = 0;
+  let count8 = 0;
+  let count9 = 0;
+  let count10 = 0;
 
-      if (data) {
-        Object.keys(data).forEach((key) => {
-          const session = data[key];
-          if (session.surgeonName) {
-            surgeonsList.push(session.surgeonName);
-          }
-        });
+  onValue(ref(this.db, 'items'), (snapshot) => {
+    const data = snapshot.val();
 
-        this.drLynnCount = Object.values(data).filter((session: any) =>
-          session.surgeonName === 'Dr.Lynn Abbidi'
-        ).length;
+    if (data) {
+      Object.keys(data).forEach((key) => {
+        const session = data[key];
 
-        this.surgeons = surgeonsList;
-        console.log("Surgeons loaded:", this.surgeons);
-      } else {
-        console.log("No data found");
-      }
-    });
-  }
+        if (session.surgeonName) {
+          surgeonsList.push(session.surgeonName);
+
+          if (session.surgeonName === 'Dr.Dareen Alherbawi') count1++;
+          else if (session.surgeonName === 'Dr.Heba Naba') count2++;
+          else if (session.surgeonName === 'Dr.Lynn Abbidi') count3++;
+          else if (session.surgeonName === 'Dr.Mohammad Ahsan') count4++;
+          else if (session.surgeonName === 'Dr.Ahmad Salim') count5++;
+          else if (session.surgeonName === 'Dr.Layla Abdalla') count6++;
+          else if (session.surgeonName === 'Dr.Kiran Mostafa') count7++;
+          else if (session.surgeonName === 'Dr.Faheem Tadros') count8++;
+          else if (session.surgeonName === 'Dr.Zakarya Hilal') count9++;
+          else if (session.surgeonName === 'Dr. Ali Hasan') count10++;
+        }
+
+ this.drDareenCount = count1;
+this.drHebaCount = count2;
+this.drLynnCount = count3;
+this.drMohammadCount = count4;
+this.drAhmadCount = count5;
+this.drLaylaCount = count6;
+this.drKiranCount = count7;
+this.drFaheemCount = count8;
+this.drZakaryaCount = count9;
+this.drAliCount = count10;
+
+      });
+
+      this.surgeons = surgeonsList;
+
+   
+    } else {
+      console.log("No data found");
+    }
+  });
+}
 }
